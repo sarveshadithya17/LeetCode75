67da0bf68571dd5df592e9134b81910d7b43ff9e
Sarvesh Adithya J <133513238+sarveshadithya17@users.noreply.github.com>
2023-12-19 19:02:28 +0530

    Create Find Pivot Index.py

---
+++ Find Pivot Index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        for i in range(len(nums)):
+            left_sum = sum(nums[:i])
+            right_sum = sum(nums[i + 1:])
+            
+            if left_sum == right_sum:
+                return i
+        
+        return -1
